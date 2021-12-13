0f011f24777ca34a18fd583c47eefef9b4e626c6
hamitmizrak <hamitmizrak@gmail.com>
2021-12-13 19:59:32 +0300

    sql create readMe

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+Önce maven project ==> internal ==> quickstart
+Hibernate
+1-Mysql 
+2-postgresql
+3-Log4j
---
+++ src/main/java/com/bilgeadam/util/allsql.sql
@@ -0,0 +1,8 @@
+-- database oluşturmak
+CREATE SCHEMA `bilge_adam` DEFAULT CHARACTER SET utf8 ;
+
+-- birden fazla database varsa bunu kullanmak zorundayız.
+use bilge_adam;
+
+-- database sil
+DROP DATABASE `bilge_adam`;
