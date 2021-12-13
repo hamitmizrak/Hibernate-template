988bff2b797f96b0f833e5a6660e1ffd3482c955
hamitmizrak <hamitmizrak@gmail.com>
2021-12-13 20:00:31 +0300

    SessionFactory ==> Singleton design pattern

---
+++ src/main/java/com/bilgeadam/util/HibernateUtil.java
@@ -0,0 +1,31 @@
+package com.bilgeadam.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	private static final SessionFactory sessionFactory = sessionFactoryHibernate();
+	
+	// method
+	private static SessionFactory sessionFactoryHibernate() {
+		try {
+			// instance
+			Configuration configuration = new Configuration();
+			
+			// entity classlarımızı buraya ekleyeceğiz
+			// configuration.addAnnotatedClass(StudentEntity.class);
+			
+			SessionFactory factory = configuration.configure("hibernate.cfg.xml").buildSessionFactory();
+			return factory;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	// dış dünyada bununla bu classa erişim sağlayabileceğim.
+	public static SessionFactory getSessionfactory() {
+		return sessionFactory;
+	}
+	
+}
